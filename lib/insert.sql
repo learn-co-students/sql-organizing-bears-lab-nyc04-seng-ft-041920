14024fd415d0a52cc60ce3910fb19f763021a0de
senadakadric <skadric@fordham.edu>
2020-05-20 13:17:27 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+
+-- INSERT INTO tablename (column names to be filled w/ data) VALUES (list of values corresponding to each column name)
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Mr. Chocolate', 23, "M", "Brown", "nice", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Rowdy', 20, "M", "Brown", "mean", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 23, "M", "Brown", "nice", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 23, "M", "Brown", "nice", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Melissa', 23, "M", "Brown", "nice", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Grinch', 23, "M", "Brown", "nice", true);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ('Wendy', 23, "F", "Brown", "nice", false);
+
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 23, "M", "Brown", "nice", true);
