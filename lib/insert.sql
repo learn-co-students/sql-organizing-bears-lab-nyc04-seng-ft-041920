e6f88d90b5cdb7919d337a79d15c6f02aed7c363
iamkevn <iamkevnrodriguez@gmail.com>
2020-05-20 20:26:50 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Mr.Chocolate', 20, 'M', 'brown', 'happy', 0);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Rowdy', 21, 'M', 'black', 'timid', 1);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Tabitha', 17, 'F', 'tan', 'sweet', 1);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Sergeant Brown', 19, 'M', 'brown', 'assertive', 1);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Melissa', 16, 'F', 'black', 'mean', 0);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Grinch', 27, 'M', 'green', 'bitter', 0);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    ('Wendy', 20, 'F', 'red', 'nice', 1);
+
+INSERT INTO bears
+    (name, age, gender, color, temperament, alive)
+VALUES
+    (NULL, 20, 'M', 'white', 'angry', 1);
